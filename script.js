bcdfbd8efeb7f13305fa169ac9c9eca592854e96
Nimesh19666 <gujarinimesh-cmpn@atharvacoe.ac.in>
2023-07-03 13:17:44 +0530

    Uploading all files

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Hello");
+const startbutton = document.querySelector(".start");
+const cardplayer = document.querySelector(".player");
+const cardbot = document.querySelector(".bot");
+
